88a5d73690bb81ed7233957690f84950ff8fcf34
tomocrafter <tomoppeko@gmail.com>
2021-11-28 19:50:18 +0900

    flashlessをセットアップ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react';
-import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import theme from '../src/theme';
-import { ColorModeScript } from '@chakra-ui/react';
+import { FlashlessScript } from 'chakra-ui-flashless';
 
 export default class Document extends NextDocument {
-  static getInitialProps(ctx: DocumentContext) {
-    return NextDocument.getInitialProps(ctx);
-  }
-
   render() {
     return (
       <Html lang='ja'>
         <Head />
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <FlashlessScript theme={theme} />
           <Main />
           <NextScript />
         </body>
